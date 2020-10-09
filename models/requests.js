1b3bd2bf5fe1f10a9205249d6f029318fe6931a6
adhish <adhishmalviya@gmail.com>
2020-10-09 23:21:08 +0530

    mongo schema made unstrict

--- models/requests.js
+++ models/requests.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const requestSchema = new mongoose.Schema({});
+const requestSchema = new mongoose.Schema({}, { strict: false });
 const RequestFare = mongoose.model("RequestFare", requestSchema);
 
 exports.RequestFare = RequestFare;
